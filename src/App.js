c6aac35de68bc3ff95133d496091fab64880ef3c
joseph <michealkinuthia.m.k@gmail.com>
2024-02-11 00:01:53 +0300

    css styling

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>TEAM MAMIO </h1>
       <header className="App-header">
+
+      <h4>COOKING</h4>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+     
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        <p>Welcome to **Bot Battlr**, the one and only spot in the known universe where you can custom build your own Bot Army! This is our app</p>
         </a>
       </header>
     </div>
