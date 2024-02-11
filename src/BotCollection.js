c4add331f343d123575b008e6ecdb84874ed0216
joseph <michealkinuthia.m.k@gmail.com>
2024-02-11 15:24:50 +0300

    container,classname

--- src/BotCollection.js
+++ src/BotCollection.js
@@ -12,26 +12,27 @@
     return( 
         <div>
             <h3>Bots Collection</h3>
-
+            <div className="bot-container">
             {Object.entries(groupedBots).map(([bot_class, botsGroup]) => (
-        <div key={bot_class}>
+        <div key={bot_class}  className="bot-group">
           <h4>Bot Class: {bot_class}</h4>
 
             <ol>
                 {botsGroup.map(bot=>(
-                    <li key={bots.id} >
+                    <li key={bot.id} >
                         <h2>{bot.title}</h2>
                         <p>ID:{bot.id}</p>
 <p>Name: {bot.name}</p>
 <p>Health: {bot.health}</p>
 <p>Armor: {bot.armor}</p>
 <p>Bot_class: {bot.bot_class}</p>
-<img src={bot.avatar_url} alt={bot.name} style={{width: '500px', height: '700px'}} />
+<img src={bot.avatar_url} alt={bot.name} style={{width: '100px', height: '200px'}} />
                     </li>
                 ))}
             </ol>
         </div>
              ) )}
+             </div>
              </div>
     );
 };
